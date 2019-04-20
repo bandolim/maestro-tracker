053ac01fe8e6a6b8534736a0fa89794c1b7bb08a
Mateus Athaydes Martins <mateus.athaydes@bandolim.ba>
2019-04-20 14:03:06 -0300

    added last_location method to tracker

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,6 +1,10 @@
 class Tracker < ApplicationRecord
-  has_many :tracker_locations
+  has_many :locations, class_name: 'TrackerLocation'
 
   validates_presence_of :equipment_name
   validates_uniqueness_of :tracker_eui
+
+  def last_location
+    return locations.order(id: :desc).first
+  end
 end
