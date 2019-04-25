03f85e360ce1e0bb8ac755b5c0b7945e09caee61
Mateus Athaydes Martins <mateus.athaydes@bandolim.ba>
2019-04-25 19:29:02 -0300

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,108 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+tracker = Tracker.create!({
+  "equipment_name": "Massey Fergusson AG12",
+  "tracker_eui": "eui38",
+  "serial_number": "9943- 8851-AJEK9999"
+})
+
+tracker.locations.create!({
+  "latitude": -29.880387, "longitude": -51.1895582, "battery_level": 582.90
+})
+
+tracker = Tracker.create!({
+  "equipment_name": "Massey Fergusson AG66",
+  "tracker_eui": "eui66",
+  "serial_number": "9943-8851-AJEK9999"})
+
+tracker.locations.create!({
+  "latitude": -29.879662, "longitude": -51.1909782, "battery_level": 782.90
+})
+
+tracker = Tracker.create!({
+  "equipment_name": "Massey Fergusson AG08",
+  "tracker_eui": "eui08",
+  "serial_number": "9943-8851-AJEK9999"})
+
+tracker.locations.create!({
+  "latitude": -29.879083, "longitude": -51.1912632, "battery_level": 632.90
+})
+
+tracker = Tracker.create!({
+  "equipment_name": "VALTRA AI31",
+  "tracker_eui": "eui31",
+  "serial_number": "9943-8851-AJEK9999"})
+
+tracker.locations.create!({
+  "latitude": -29.880317, "longitude": -51.1912912, "battery_level": 912.90
+})
+
+tracker = Tracker.create!({
+  "equipment_name": "VALTRA AI31",
+  "tracker_eui": "eui55",
+  "serial_number": "9943-8851-AJEK9999"})
+
+tracker.locations.create!({
+  "latitude": -29.880556, "longitude": -51.1908315, "battery_level": 315.90
+})
+
+tracker = Tracker.create!({
+  "equipment_name": "Massey Fergusson AG13",
+  "tracker_eui": "eui67",
+  "serial_number": "9943-8851-AJEK9999"})
+
+tracker.locations.create!({
+  "latitude": -29.879673, "longitude": -51.190968, "battery_level": 968.90
+})
+
+tracker = Tracker.create!({
+  "equipment_name": "Massey Fergusson AG64",
+  "tracker_eui": "eui64",
+  "serial_number": "9943-8851-AJEK9999"})
+
+tracker.locations.create!({
+  "latitude": -29.879647, "longitude": -51.191286, "battery_level": 286.90
+})
+
+tracker = Tracker.create!({
+  "equipment_name": "CHALLENGER 0139",
+  "tracker_eui": "eui43",
+  "serial_number": "9943-8851-AJEK9999"})
+
+tracker.locations.create!({
+  "latitude": -29.879434, "longitude": -51.190555, "battery_level": 555.90
+})
+
+tracker = Tracker.create!({
+  "equipment_name": "CHALLENGER 902",
+  "tracker_eui": "eui49",
+  "serial_number": "9943-8851-AJEK9999"})
+
+tracker.locations.create!({
+  "latitude": -29.880497, "longitude": -51.1910072, "battery_level": 72.90
+})
+
+tracker = Tracker.create!({
+  "equipment_name": "CHALLENGER 982",
+  "tracker_eui": "eui10",
+  "serial_number": "9943-8851-AJEK9999"})
+
+tracker.locations.create!({
+  "latitude": -29.880107, "longitude": -51.1908462, "battery_level": 462.90
+})
+
+tracker = Tracker.create!({
+  "equipment_name": "VALTRA AG12",
+  "tracker_eui": "eui61",
+  "serial_number": "9943-8851-AJEK9999"})
+
+tracker.locations.create!({
+  "latitude": -29.87961, "longitude": -51.1903835, "battery_level": 835.90
+})
+
+tracker = Tracker.create!({
+  "equipment_name": "VALTRA O102",
+  "tracker_eui": "eui23",
+  "serial_number": "9943-8851-AJEK9999"})
+
+tracker.locations.create!({
+  "latitude": -29.880235, "longitude": -51.1899535, "battery_level": 535.90
+})
